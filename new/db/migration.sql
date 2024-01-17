3b288c5e8cd884f67353b466e2a4a298c39b8326
universejino <universejino@gmail.com>
2024-01-17 14:48:17 +0530

    pancard

---
+++ new/db/migration.sql
@@ -0,0 +1,3 @@
+update macho_user_page_acceses set menu_icon='fa fas fa-receipt rupee-icon' where id=27;
+update macho_user_page_acceses set menu_icon='fa fas fa-coins rupee-icon' where id=28;
+update macho_user_page_acceses set menu_icon='fa fas fa-credit-card rupee-icon' where id=25;
