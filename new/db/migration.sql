34874ad09862cc7a5ec97afc6f971cfb815adcb0
galaxysaravanan <saravananperumal2014@gmail.com>
2024-02-24 16:31:50 +0530

    new

--- new/db/migration.sql
+++ new/db/migration.sql
@@ -2,6 +2,7 @@ update macho_user_page_acceses set menu_icon='fa fas fa-receipt rupee-icon' wher
 update macho_user_page_acceses set menu_icon='fa fas fa-coins rupee-icon' where id=28;
 update macho_user_page_acceses set menu_icon='fa fas fa-credit-card rupee-icon' where id=25;
 ALTER TABLE macho_test_type ADD sample_type VARCHAR(100) default NULL;
+ALTER TABLE macho_users ADD colour VARCHAR(10) NOT NULL default value 'theme1';
 update macho_test_type set method=NULL where method='';
 
 
