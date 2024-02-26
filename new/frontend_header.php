3d3e40c7d2ac63c6eeb3d9758aa7f7e112e6c547
Vanithavanitha <vanitha171299@gmail.com>
2024-02-26 17:40:26 +0530

    update

--- new/frontend_header.php
+++ new/frontend_header.php
@@ -7,14 +7,14 @@
             <div class="d-none d-sm-flex align-items-center me-3">
               <div> <i class="las la-phone"></i>
               </div>
-              <div>Emergency Line: <a class="text-black" href="tel:+912345678900"> +91-234-567-8900</a>
+              <div>Phone: <a class="text-black" href="contact:+919500936096"> +919500936096</a>
               </div>
             </div>
             <div class="d-none d-md-flex align-items-center me-3">
               <div> <i class="las la-map-marker"></i>
               </div>
               <div>
-                <div>Location: Country USA</div>
+                <div>Location: Nagercoil</div>
               </div>
             </div>
             <div class="d-none d-lg-flex align-items-center">
@@ -56,131 +56,24 @@
             <div class="collapse navbar-collapse" id="navbarNav">
               <ul class="navbar-nav ms-auto me-auto position-relative">
                 <!-- Home -->
-                <li class="nav-item dropdown"> <a class="nav-link active dropdown-toggle" href="#" data-bs-toggle="dropdown">Home</a>
-                  <ul class="dropdown-menu">
-                    <li><a href="index.html">Homepage One</a>
-                    </li>
-                    <li><a href="index-2.html">Homepage Two</a>
-                    </li>
-                    <li><a href="index-3.html">Homepage Three</a>
-                    </li>
-                  </ul>
-                </li>
-                <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Pages</a>
-                  <ul class="dropdown-menu">
-                    <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">About Us</a>
-                      <ul class="dropdown-menu">
-                        <li><a href="about-us.html">About Us 1</a>
-                        </li>
-                        <li><a href="about-us-2.html">About Us 2</a>
-                        </li>
-                      </ul>
-                    </li>
-                    <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">Team</a>
-                      <ul class="dropdown-menu">
-                        <li><a href="team.html">Our Team</a>
-                        </li>
-                        <li><a href="team-single.html">Team Single</a>
-                        </li>
-                      </ul>
-                    </li>
-                    <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">Contact Us</a>
-                      <ul class="dropdown-menu">
-                        <li><a href="contact.html">Contact us 1</a>
-                        </li>
-                        <li><a href="contact-2.html">Contact us 2</a>
-                        </li>
-                      </ul>
-                    </li>
-                    <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">Element</a>
-                      <ul class="dropdown-menu">
-                        <li><a href="shortcode-about.html">About</a>
-                        </li>
-                        <li> <a href="shortcode-counter.html">Counter</a>
-                        </li>
-                        <li> <a href="shortcode-feature.html">Feature Box</a>
-                        </li>
-                        <li><a href="shortcode-pricing.html">Pricing Table</a>
-                        </li>
-                        <li> <a href="shortcode-progress-bar.html">Progress Bar</a>
-                        </li>
-                        <li> <a href="shortcode-services.html">Services</a>
-                        </li>
-                        <li> <a href="shortcode-team.html">Team</a>
-                        </li>
-                        <li> <a href="shortcode-testimonial.html">Testimonial</a>
-                        </li>
-                      </ul>
-                    </li>
-                    <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">Utilities</a>
-                      <ul class="dropdown-menu">
-                        <li><a href="faq.html">Faq</a>
-                        </li>
-                        <li><a href="pricing.html">Pricing</a>
-                        </li>
-                        <li> <a href="login.html">Login</a>
-                        </li>
-                        <li><a href="coming-soon.html">Coming Soon</a>
-                        </li>
-                        <li><a href="error-404.html">404 Error</a>
-                        </li>
-                      </ul>
-                    </li>
-                  </ul>
-                </li>
-                <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Shop</a>
-                  <ul class="dropdown-menu">
-                    <li><a href="product-grid.html">Product Grid</a>
-                    </li>
-                    <li><a href="product-list.html">Product List</a>
-                    </li>
-                    <li><a href="product-single.html">Product Single</a>
-                    </li>
-                    <li><a href="product-cart.html">Product Cart</a>
-                    </li>
-                    <li><a href="product-checkout.html">Product Checkout</a>
-                    </li>
-                  </ul>
-                </li>
-                <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Services</a>
-                  <ul class="dropdown-menu">
-                    <li><a href="pathologycam-testing.html">Pathologycam Testing</a>
-                    </li>
-                    <li><a href="biochemistry-solution.html">Biochemistry Solution</a>
-                    </li>
-                    <li><a href="chemical-research.html">Chemical Research</a>
-                    </li>
-                    <li><a href="pharmaceutical-research.html">Pharmaceutical Research</a>
-                    </li>
-                    <li><a href="diabetes-testing.html">Diabetes Testing</a>
-                    </li>
-                  </ul>
-                </li>
-                <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">
-                      Research
-                    </a>
-                  <ul class="dropdown-menu">
-                    <li><a href="research-style-1.html">Research Style One</a>
-                    </li>
-                    <li><a href="research-single.html">Research Single</a>
-                    </li>
-                  </ul>
-                </li>
-                <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">News</a>
-                  <ul class="dropdown-menu">
-                    <li><a href="blog-left-sidebar.html">Blog left sidebar</a>
-                    </li>
-                    <li><a href="blog-right-sidebar.html">Blog right sidebar</a>
-                    </li>
-                    <li><a href="blog-single.html">Blog Single</a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-            <div class="right-nav align-items-center d-flex justify-content-end">
-              <div class="search-icon d-sm-inline-block d-none"> <a id="search" href="javascript:void(0)"><i class="las la-search"></i></a>
-              </div>
+                <li class="nav-item dropdown"> <a class="nav-link active dropdown" href="#" data-bs-toggle="dropdown">Home</a></li>
+                 
+				 <li class="nav-item dropdown"> <a class="nav-link active dropdown" href="#" data-bs-toggle="dropdown">About Us</a></li>
+				 
+				 <li class="nav-item dropdown"> <a class="nav-link active dropdown" href="#" data-bs-toggle="dropdown">Products</a></li>
+				 
+				 <li class="nav-item dropdown"> <a class="nav-link active dropdown" href="#" data-bs-toggle="dropdown">Services</a></li>
+				 
+				 <li class="nav-item dropdown"> <a class="nav-link active dropdown" href="#" data-bs-toggle="dropdown">Support</a></li>
+				 
+				 <li class="nav-item dropdown"> <a class="nav-link active dropdown" href="#" data-bs-toggle="dropdown">Contact</a></li>
+				 
+				 <li class="nav-item dropdown"> <a class="nav-link active dropdown" href="#" data-bs-toggle="dropdown">Enquiry</a></li>
+				 
+				 
+				 
+				 
+				 
               <div class="ms-sm-3 me-md-5">
                 <!-- <div class="cart">
                   <a href="#" id="header-cart-btn" data-bs-toggle="modal" data-bs-target="#cartModal"> <span class="white-bg px-2 py-1 shadow-sm" data-cart-items="2">
